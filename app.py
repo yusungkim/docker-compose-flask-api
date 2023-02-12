47d17e4222ee06d4e215c1ee1f0db8ee44ec29ca
YusungKim <yusungkim@me.com>
2023-02-12 11:58:19 +0900

    cors

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
 import werkzeug
 import time
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.errorhandler(werkzeug.exceptions.BadRequest)
 def handle_bad_request(e):
